6ec0b925140eb7ff75dd9ca9a6788dd7bc2fcea2
Notkabene <len1618@yandex.ru>
2024-06-03 17:52:00 +0300

    add modal, portal, change header

---
+++ src/shared/ui/Modal/Modal.test.tsx
@@ -0,0 +1,9 @@
+// import { render, screen } from '@testing-library/react';
+// import { Modal } from 'shared/ui/Modal/Modal';
+//
+// describe('Modal', () => {
+//     test('Test render', () => {
+//         // render(<Modal><span>TEST</span></Modal>);
+//         // expect(screen.getByText('TEST')).toBeInTheDocument();
+//     });
+// });
