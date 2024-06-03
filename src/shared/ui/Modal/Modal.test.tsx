733bf515b65e45a3758884456515946e3c146d97
Notkabene <len1618@yandex.ru>
2024-06-03 17:59:48 +0300

    fix problems

--- src/shared/ui/Modal/Modal.test.tsx
+++
@@ -1,9 +0,0 @@
-// import { render, screen } from '@testing-library/react';
-// import { Modal } from 'shared/ui/Modal/Modal';
-//
-// describe('Modal', () => {
-//     test('Test render', () => {
-//         // render(<Modal><span>TEST</span></Modal>);
-//         // expect(screen.getByText('TEST')).toBeInTheDocument();
-//     });
-// });
