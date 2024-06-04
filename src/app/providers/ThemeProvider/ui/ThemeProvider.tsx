37c737691fd38c498398757d31eec9b4321a5094
Notkabene <len1618@yandex.ru>
2024-06-04 13:41:47 +0300

    fix theme for modal

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,4 +1,6 @@
-import React, { FC, useMemo, useState } from 'react';
+import React, {
+    FC, useEffect, useMemo, useState,
+} from 'react';
 import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from '../lib/ThemeContext';
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
@@ -14,6 +16,14 @@ const ThemeProvider: FC<ThemeProviderProps> = ({ children, initialTheme }) => {
         theme,
         setTheme,
     }), [theme]);
+
+    useEffect(() => {
+        document.body.classList.add(theme);
+
+        return () => {
+            document.body.classList.remove(theme);
+        };
+    }, [theme]);
 
     return (
         <ThemeContext.Provider value={defaultProps}>
